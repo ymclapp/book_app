95db0f07ec03496be23ba4ae3a2f0c4bafee3659
ymclapp <ymclapp@yahoo.com>
2020-06-22 18:09:16 -0500

    worked on the books constructor

--- public/styles/modules/books.js
+++ public/styles/modules/books.js
@@ -6,26 +6,26 @@ const client = require('../util/db');
 function booksHandler(request,response) {
   const author = request.query.latitude;
   const title =  request.query.longitude;
-  const url = 'https://api.weatherbit.io/v2.0/forecast/daily';
-  superagent.get(url).query({
-    key:  process.env.WEATHER_API_KEY,
-    lon: lon,
-    lat: lat
-  }).then((weathResponse)=>{
-    let weatherData = weathResponse.body.data;
-    let dailyResults = weatherData.map(dailyWeather=>{
-      return new Weather(dailyWeather);
+  const url = 'https://www.googleapis.com/books/v1/volumes';
+  superagent.get(url)
+    .query({
+      key:  process.env.BOOKS_API_KEY,
+      q: +in${request.body.search}:${request.body.query},
+    }).then((booksResponse) => {
+      let booksData = json.parse(booksResponse.text);
+      let dailyResults = booksData.map(dailyWeather=>{
+        return new Book(dailyWeather);
+      });
+      response.send(dailyResults);
+    }).catch((err)=>{
+      errorHandler(err,request,response);
     });
-    response.send(dailyResults);
-  }).catch((err)=>{
-    errorHandler(err,request,response);
-  });
 }
 
-function Weather(weatherData) {
+function Book(weatherData) {
   this.forecast = weatherData.weather.description;
   this.time = new Date (weatherData.valid_date).toDateString();
 }
 
 
-module.exports = weatherHandler;
+module.exports = booksHandler;
